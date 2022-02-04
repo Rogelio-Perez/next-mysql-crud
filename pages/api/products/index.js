65816cb20314ae00ae2ab2ee5736bf7219cedde7
roger <perezmenarogelio@gmail.com>
2022-02-04 16:32:39 -0600

    list data

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -3,19 +3,28 @@ import { pool } from "../../../config/db"
 export default async function handler(req, res) {
   switch (req.method) {
     case "GET":
-      return res.status(200).json("Getting a product")
+      return await getProrducts(req, res)
     case "POST":
-      const { name, description, price } = req.body
-
-      const [result] = await pool.query("INSERT INTO product SET ?", {
-        name,
-        description,
-        price,
-      })
-
-      console.log(result)
-      return res
-        .status(200)
-        .json({ name, price, description, id: result.insertId })
+      return await saveProduct(req, res)
   }
+}
+
+const getProrducts = async (req, res) => {
+  const [result] = await pool.query("SELECT * FROM product")
+  console.log(result)
+
+  return res.status(200).json(result)
+}
+
+const saveProduct = async (req, res) => {
+  const { name, description, price } = req.body
+
+  const [result] = await pool.query("INSERT INTO product SET ?", {
+    name,
+    description,
+    price,
+  })
+
+  console.log(result)
+  return res.status(200).json({ name, price, description, id: result.insertId })
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,33 @@
-import React from "react"
-
+import axios from "axios"
 import { ProductForm } from "../components/ProductForm"
 
-function HomePage() {
+function HomePage({ products }) {
+  console.log(products)
   return (
     <div>
       <ProductForm />
+
+      {products.map((product) => (
+        <div key={product.id}>
+          <h1>{product.name}</h1>
+          <p>{product.description}</p>
+          <p>{product.price}</p>
+        </div>
+      ))}
     </div>
   )
+}
+
+export const getServerSideProps = async (context) => {
+  const { data: products } = await axios.get(
+    "http://localhost:3000/api/products"
+  )
+
+  return {
+    props: {
+      products,
+    },
+  }
 }
 
 export default HomePage
