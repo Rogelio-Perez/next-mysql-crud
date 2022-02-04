f4130663575bb858dbc548b565caada876ce396f
roger <perezmenarogelio@gmail.com>
2022-02-04 16:51:09 -0600

    save data from react

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,29 +1,56 @@
 import axios from "axios"
+import { useState } from "react"
 
 export function ProductForm() {
+  const [product, setProduct] = useState({
+    name: "",
+    description: "",
+    price: 0,
+  })
+
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const res = await axios.post("/api/products", {
-      name: "product 1",
-      description: "Some prodcut",
-      price: 10000,
-    })
+    const res = await axios.post("/api/products", product)
     console.log(res)
   }
 
+  const handleChange = ({ target: { name, value } }) =>
+    setProduct({ ...product, [name]: value })
+
   return (
-    <div className="bg-gray-300">
-      <form onSubmit={handleSubmit}>
+    <div className="w-full max-w-xs">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
         <label htmlFor="name">Name:</label>
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          onChange={handleChange}
+          className="shadow border rounded py-2 px-3 text-gray-700"
+        />
 
         <label htmlFor="price">Price:</label>
-        <input type="text" name="price" id="price" />
+        <input
+          type="text"
+          name="price"
+          id="price"
+          onChange={handleChange}
+          className="shadow border rounded py-2 px-3 text-gray-700"
+        />
 
         <label htmlFor="description">Description:</label>
-        <textarea name="description" rows="2"></textarea>
+        <textarea
+          name="description"
+          rows="2"
+          onChange={handleChange}
+          className="shadow border rounded py-2 px-3 text-gray-700"
+        ></textarea>
 
-        <button>Save product</button>
+        <button className="bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded focus:outline-none focus:shadow-outline font-bold text-white">
+          Save product
+        </button>
       </form>
     </div>
   )
